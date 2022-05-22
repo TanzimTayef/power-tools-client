ca12196f000081c6e7b464c424eeca2883946595
TanzimTayef <tayef464@gmail.com>
2022-05-22 09:50:36 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is power tools</h1>
-      <button class="btn">Button</button>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </div>
   );
 }
