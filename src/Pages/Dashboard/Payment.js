5a113f4fe73a663134679cd0d14ab35a1a8ecac5
TanzimTayef <tayef464@gmail.com>
2022-05-25 08:09:14 +0600

    added payment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe('pk_test_51L3997CpAU3eJINqXFGJT7xiHIi6NVaQSEwQOZUZMM2wmPQXa1DPiWs0g5cMpioRnqfkuKNciCC6E5kVcbSd4SPw00bLd92L6W');
 
 const Payment = () => {
-    const { id } = useParams();
-    const url = `http://localhost:5000/orders/${id}`;
-    const {data: order, isLoading} = useQuery(["order",id], () => fetch(url).then(res => res.json()))
-    
-    if (isLoading) {
-        return <p>Loading....</p>
-    }
+  const { id } = useParams();
+  const url = `http://localhost:5000/orders/${id}`;
 
-    return (
-        <div>
-            <h1>Play pay for: {id}</h1>
+  const { data: orders, isLoading } = useQuery(["orders", id], () =>
+    fetch(url, {
+      method: "GET",
+      // headers: {
+      //     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      // }
+    }).then((res) => res.json())
+  );
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
+
+  return (
+    <div>
+      <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div class="card-body">
+                  <p className="text-success font-bold">Hello, { orders.personName}</p>
+          <h2 class="card-title">Please Pay for  { orders.toolsName}</h2>
+          <p>
+                      Your Ordered: <span className="text-primary text-xl">{orders.quantity}</span> pieces
+          </p>
+                  <p>Please pay: ${orders.price }</p>
         </div>
-    );
+      </div>
+      <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+                        <CheckoutForm orders={orders} />
+            </Elements>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
